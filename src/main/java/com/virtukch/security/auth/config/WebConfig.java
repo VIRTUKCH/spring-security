1f87747d96662f780ff008d1211bff0a1941dda4
Virtus_Chae <ybd4731@gmail.com>
2024-11-01 20:50:23 +0900

    docs: JavaDoc 수정

--- src/main/java/com/virtukch/security/auth/config/WebConfig.java
+++ src/main/java/com/virtukch/security/auth/config/WebConfig.java
@@ -9,33 +9,63 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 웹 관련 설정을 위한 구성 클래스입니다. 정적 자원 처리 및 보안 필터 등록을 포함합니다.
+ * 커스텀 필터를 설정하고, 그에 맞는 경로를 설정합니다.
+ * @author Virtus_Chae
+ */
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    // 정적 자원에 접근을 허용하게 하기 위함
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static/", "classpath:/public/",
-            "classpath:/", "classpath:/resources/", "classpath:/META-INF/resources/",
-            "classpath:/META-INF/resources/webjars/"};
+    // 정적 자원을 제공하기 위한 경로
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+        "classpath:/static/",
+        "classpath:/public/",
+        "classpath:/",
+        "classpath:/resources/",
+        "classpath:/META-INF/resources/",
+        "classpath:/META-INF/resources/webjars/"
+    };
 
+    /**
+     * 정적 자원을 제공하기 위한 리소스 핸들러를 추가합니다.
+     *
+     * @param registry 리소스 핸들러 레지스트리
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 
+    /**
+     * HeaderFilter 를 등록하고, 지정된 URL 패턴 및 우선순위를 설정합니다.
+     *
+     * @return HeaderFilter 가 설정된 FilterRegistrationBean
+     */
     @Bean
     public FilterRegistrationBean<HeaderFilter> getFilterRegistrationBean(){
         FilterRegistrationBean<HeaderFilter> registrationBean = new FilterRegistrationBean<>(createHeaderFilter());
-        registrationBean.setOrder(Integer.MIN_VALUE);
-        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(Integer.MIN_VALUE); // 필터 우선순위를 최하로 설정
+        registrationBean.addUrlPatterns("/*"); // 모든 URL 패턴에 필터 적용
         return registrationBean;
     }
 
+    /**
+     * HeaderFilter 의 새 인스턴스를 생성하고 반환합니다.
+     *
+     * @return 새 HeaderFilter 인스턴스
+     */
     @Bean
     public HeaderFilter createHeaderFilter(){
         return new HeaderFilter();
     }
 
+    /**
+     * JwtTokenInterceptor 의 새 인스턴스를 생성하고 반환합니다.
+     *
+     * @return 새 JwtTokenInterceptor 인스턴스
+     */
     @Bean
     public JwtTokenInterceptor jwtTokenInterceptor(){
         return new JwtTokenInterceptor();
--- src/main/java/com/virtukch/security/auth/config/WebSecurityConfig.java
+++ src/main/java/com/virtukch/security/auth/config/WebSecurityConfig.java
@@ -21,6 +21,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+/**
+ * 작업을 수행할 인코더, 핸들러 등의 Bean 을 정의합니다.
+ *
+ * @author Virtus_Chae
+ */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
@@ -29,46 +34,51 @@ public class WebSecurityConfig {
     /**
      * 1. 정적 자원에 대한 인증된 사용자의 접근을 설정하는 메서드
      *
-     * @return WebSeruciryCusomizer
-     * */
+     * @return WebSecurityCustomizer
+     */
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     /**
      * security filter chain 설정
      *
      * @return SecurityFilterChain
-     * */
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(jwtAuthorizationFilter(), BasicAuthenticationFilter.class)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .formLogin(form -> form.disable())
-                .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .httpBasic(basic -> basic.disable());
+            .addFilterBefore(jwtAuthorizationFilter(), BasicAuthenticationFilter.class)
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .addFilterBefore(customAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter.class)
+            .httpBasic(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
 
     /**
-     * 3. Authentization의 인증 메서드를 제공하는 매니저로 Provider의 인터페이스를 의미한다.
+     * 3. Authentication 의 인증 메서드를 제공하는 매니저로 Provider 의 인터페이스를 의미한다.
+     *
      * @return AuthenticationManager
-     * */
+     */
     @Bean
-    public AuthenticationManager authenticationManager(){
+    public AuthenticationManager authenticationManager() {
         return new ProviderManager(customAuthenticationProvider());
     }
 
     /**
-     * 4. 사용자의 아이디와 패스워드를 DB와 검증하는 handler이다.
+     * 4. 사용자의 아이디와 패스워드를 DB와 검증하는 handler 이다.
+     *
      * @return CustomAuthenticationProvider
-     * */
+     */
     @Bean
-    public CustomAuthenticationProvider customAuthenticationProvider(){
+    public CustomAuthenticationProvider customAuthenticationProvider() {
         return new CustomAuthenticationProvider();
     }
 
@@ -76,19 +86,21 @@ public class WebSecurityConfig {
      * 5. 비밀번호를 암호화 하는 인코더
      *
      * @return BCryptPasswordEncoder
-     * */
+     */
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     /**
      * 6. 사용자의 인증 요청을 가로채서 로그인 로직을 수행하는 필터
+     *
      * @return CustomAuthenticationFilter
-     * */
+     */
     @Bean
-    public CustomAuthenticationFilter customAuthenticationFilter(){
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager());
+    public CustomAuthenticationFilter customAuthenticationFilter() {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(
+            authenticationManager());
         customAuthenticationFilter.setFilterProcessesUrl("/login");
         customAuthenticationFilter.setAuthenticationSuccessHandler(customAuthLoginSuccessHandler());
         customAuthenticationFilter.setAuthenticationFailureHandler(customAuthFailUserHandler());
@@ -97,32 +109,32 @@ public class WebSecurityConfig {
     }
 
     /**
-     * 7. spring security 기반의 사용자의 정보가 맞을 경우 결과를 수행하는 handler
+     * 7. Spring Security 기반의 사용자의 정보가 맞을 경우 결과를 수행하는 handler
      *
      * @return customAuthLoginSuccessHandler
-     * */
+     */
     @Bean
-    public CutomAuthSuccessHandler customAuthLoginSuccessHandler(){
+    public CutomAuthSuccessHandler customAuthLoginSuccessHandler() {
         return new CutomAuthSuccessHandler();
     }
 
 
     /**
-     * 8. Spring security의 사용자 정보가 맞지 않은 경우 행되는 메서드
+     * 8. Spring Security 의 사용자 정보가 맞지 않은 경우 행되는 메서드
      *
      * @return CustomAuthFailUreHandler
-     * */
+     */
     @Bean
-    public CustomAuthFailUserHandler customAuthFailUserHandler(){
+    public CustomAuthFailUserHandler customAuthFailUserHandler() {
         return new CustomAuthFailUserHandler();
     }
 
     /**
      * 9. 사용자 요청시 수행되는 메소드
+     *
      * @return JwtAuthorizationFilter
-     * */
-    public JwtAuthorizationFilter jwtAuthorizationFilter(){
+     */
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
         return new JwtAuthorizationFilter(authenticationManager());
     }
-
 }
