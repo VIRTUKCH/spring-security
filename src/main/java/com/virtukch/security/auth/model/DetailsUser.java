808d0078e54a5a3648311ab944aeef30259e4088
Virtus_Chae <ybd4731@gmail.com>
2024-11-03 14:41:08 +0900

    docs: model javadoc 추가

--- src/main/java/com/virtukch/security/auth/model/DetailsUser.java
+++ src/main/java/com/virtukch/security/auth/model/DetailsUser.java
@@ -8,21 +8,46 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * 사용자 세부 정보를 나타내는 클래스입니다.
+ * 이 클래스는 Spring Security의 UserDetails 인터페이스를 구현하여
+ * 사용자 정보를 인증 및 권한 부여에 사용합니다.
+ *
+ * @author Virtus_Chae
+ */
 public class DetailsUser implements UserDetails {
 
-    private User user;
+    private User user; // 사용자 정보
 
+    /**
+     * 기본 생성자입니다.
+     */
     public DetailsUser() {
     }
 
+    /**
+     * Optional<User> 객체를 사용하여 DetailsUser 를 생성하는 생성자입니다.
+     *
+     * @param user Optional<User> 객체
+     */
     public DetailsUser(Optional<User> user) {
         this.user = user.get();
     }
 
+    /**
+     * 사용자 정보를 반환합니다.
+     *
+     * @return User 객체
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * 사용자 정보를 설정합니다.
+     *
+     * @param user User 객체
+     */
     public void setUser(User user) {
         this.user = user;
     }
@@ -30,37 +55,37 @@ public class DetailsUser implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-        user.getRoleList().forEach( role -> authorities.add(() -> role));
-        return authorities;
+        user.getRoleList().forEach(role -> authorities.add(() -> role));
+        return authorities; // 사용자 권한 목록을 반환
     }
 
     @Override
     public String getPassword() {
-        return user.getUserPass();
+        return user.getUserPass(); // 사용자의 비밀번호 반환
     }
 
     @Override
     public String getUsername() {
-        return user.getUserId();
+        return user.getUserId(); // 사용자의 ID 반환
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return true; // 계정이 만료되지 않았음을 나타냄
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return true; // 계정이 잠기지 않았음을 나타냄
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return true; // 자격 증명이 만료되지 않았음을 나타냄
     }
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return true; // 계정이 활성화되어 있음을 나타냄
     }
 }
--- src/main/java/com/virtukch/security/auth/model/dto/LoginDto.java
+++ src/main/java/com/virtukch/security/auth/model/dto/LoginDto.java
@@ -1,5 +1,10 @@
 package com.virtukch.security.auth.model.dto;
 
+/**
+ * 회원이 로그인할 때 전달하는 정보입니다. 이 DTO(Data Transfer Object)는 로그인 요청 시 사용자의 ID와 비밀번호를 포함합니다.
+ *
+ * @author Virtus_Chae
+ */
 public class LoginDto {
 
     private String id;
@@ -9,23 +14,15 @@ public class LoginDto {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getPass() {
         return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
     }
 
     @Override
     public String toString() {
         return "LoginDto{" +
-                "id='" + id + '\'' +
-                ", pass='" + pass + '\'' +
-                '}';
+            "id='" + id + '\'' +
+            ", pass='" + pass + '\'' +
+            '}';
     }
 }
